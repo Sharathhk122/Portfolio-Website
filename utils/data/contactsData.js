b6d92dda46ba5d4800ffea42d0e59882a917ec48
Sharath <sharthhk40@gmail.com>
2025-04-03 16:42:49 +0530

    Initial commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'sharathhk40@gmail.com',
+    phone: '+91 9916174655',
+    address: 'vallabhai road 2nd cross Hassan ,karnataka,INDIA,573201 ',
+    github: 'https://github.com/Sharathhk122/Sharathhk122',
+    facebook: 'https://www.facebook.com/share/1H6X9XSHvf/',
+    linkedIn: 'https://www.linkedin.com/in/sharath-h-k-174536308?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ',
+    twitter: 'https://x.com/SharathHk417289?t=MycxlBoBa1lbPSKAOk8vcQ&s=09',
+    stackOverflow: 'https://www.hackerrank.com/profile/sharathhk40',
+    devUsername: "said7388"
+}
---
+++ utils/data/projects-data.js
@@ -0,0 +1,52 @@
+export const projectsData = [
+    {
+        id: 1,
+        name: 'ChatGPT DApp',
+        description: "ChatDApp is a decentralized chatbot application built using **React**, **Web3**, and **smart contracts**, designed to provide blockchain-based membership functionality. It integrates **MetaMask**, **Web3Modal**, and **ethers.js** for secure wallet authentication and interaction with the **GPTMembership** contract deployed on a **Hardhat** local network. Users can connect their wallets, access premium chatbot features based on their membership status, and experience a Web3-powered AI conversation system. The project enhances user engagement by combining AI-powered chat with decentralized authentication and membership management.",
+        tools: ['Express',  'DeepSeek API', 'Node js', 'HTML', 'JAVASCRIPT', 'CSS', 'METAMASK', 'WEB3MODAL'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+    },
+    {
+        id: 2,
+        name: 'Human Resource Management System (HRMS)',
+        description: 'The **Human Resource Management System** is designed to streamline HR processes within an organization. It encompasses features such as employee information management, attendance tracking, leave management, and performance evaluations. The system aims to enhance operational efficiency by automating routine HR tasks, thereby reducing manual workload and minimizing errors. By providing a centralized platform for managing employee data and HR activities, the project facilitates improved decision-making and organizational productivity.',
+        tools: ["PHP","CSS","JAVACRIPT","HTML",'MYSQL'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+    },
+    {
+        id: 3,
+        name: 'Air-Canvas-with-ML',
+        description: 'The **Air Canvas** project is a Python-based application that enables users to draw on a virtual canvas using hand gestures captured via a webcam. Leveraging **OpenCV** for computer vision and **MediaPipe** for hand tracking, the program identifies hand movements and translates them into digital drawings in real-time. Users can select different colors and clear the canvas through specific gestures, providing an intuitive and interactive drawing experience without the need for physical contact with a device.',
+        tools: ['PYTHON','CV2','NUMPY','MEDIAPIPE'],
+        code: '',
+        role: 'Software Developer',
+        demo: '',
+    },
+    {
+        id: 4,
+        name: 'Resume Builder',
+        description: "This online resume builder assists job searchers in building a professional resume, making the task of creating a resume easier. Individuals must simply fill out a form that includes questions from all required sections, such as personal questions, educational questions, qualities, interests, and skills. Users' responses are kept, and the system generates a well-structured resume with customisable templates automatically. Users have the option of creating resumes in any format or file type.",
+        tools: ['HTML','CSS','JS'],
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+    }
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// },
