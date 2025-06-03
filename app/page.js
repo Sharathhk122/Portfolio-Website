6ee2dd34464f2408314e2c8718fd36660def4e4e
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-03 11:08:36 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -8,21 +7,16 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-
-
-export default async function Home() {
-
-
+export default function Home() {
   return (
-    <div suppressHydrationWarning >
+    <div>
       <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      
       <ContactSection />
     </div>
   )
-};
+}
