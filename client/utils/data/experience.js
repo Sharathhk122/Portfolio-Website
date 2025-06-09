b473d3c252050d6f5853dfb34493aff3058776de
Sharath <sharthhk40@gmail.com>
2025-06-09 10:22:54 +0530

    Updated portfolio structure with new client folder and removed old files

---
+++ client/utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Engineer ',
+    company: "IBM - Internship - Devops",
+    duration: "(Jan 2024 - Present)"
+  },
+  {
+    id: 2,
+    title: "FullStack Developer(Html,Css,Js,MERN)",
+    company: "Samagra - Internship",
+    duration: "(Jun 2025 - may 2025)"
+  },
+  {
+    id: 3,
+    title: "Self Employed",
+    company: "Code and build something in everyday.",
+    duration: "(Jan 2021 - Present)"
+  }
+]
